8916449147dc46e1f039392da90bc9b978d8752c
Rafael <rafabraga85@gmail.com>
2020-03-06 16:46:43 -0400

    lodash

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const calculoMaluco = require('./modulo');
 const log = require('./util');
+const _ = require('lodash');
 
-const nome = 'Rafael';
-
-const resultado = calculoMaluco(10, 2);
-
-// console.log('Estou em -> app.js');
-// console.log(`${nome} ${sobrenome}`);
-
-console.log(resultado);
-
-
+console.log('app.js');
 log("primeiro mensagem de log");
+log("segunda mensagem de log");
+
+const meuArray = [1, 2, 3, 4, 1, 9];
+
+const meuNovoArray = _.chunk(meuArray, 3);
+
+console.log(meuArray);
+console.log(meuNovoArray);
+
+const diferenca = _.difference(meuNovoArray[0], meuNovoArray[1]);
+
+console.log(diferenca);
+
+console.log(_.indexOf(meuArray, 10));
--- util.js
+++ util.js
@@ -9,6 +9,8 @@
 const fs = require('fs');
 const logFileName = 'log.txt';
 
+
+
 const log = (content) => {
     const currentDate = new Date();
     fs.appendFileSync(logFileName, `${currentDate} -> ${content}\n`);
