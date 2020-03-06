be40d70128c06e6d3acdf6c3b347a81a78cb3568
Rafael <rafabraga85@gmail.com>
2020-03-06 17:56:22 -0400

    chalk

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const log = require('./util');
 const _ = require('lodash');
+const  chalk = require('chalk');
 
 console.log('app.js');
 log("primeiro mensagem de log");
@@ -9,7 +10,13 @@ const meuArray = [1, 2, 3, 4, 1, 9];
 
 const meuNovoArray = _.chunk(meuArray, 3);
 
-console.log(meuArray);
+const primeiraMensagem = chalk.bold.green.inverse('Array inicial');
+const segundaMensagem = chalk.bold.red.inverse('meu novo Array');
+
+console.log(primeiraMensagem);
+console.log(chalk.blue(meuArray));
+
+console.log(segundaMensagem);
 console.log(meuNovoArray);
 
 const diferenca = _.difference(meuNovoArray[0], meuNovoArray[1]);
