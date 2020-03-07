4fcbb6b8b95188531bd8983a8dcc50804c274e3c
Rafael <rafabraga85@gmail.com>
2020-03-07 18:37:36 -0400

    es6 2

--- 02-PWN/exemplos/es6.js
+++ 02-PWN/exemplos/es6.js
@@ -4,8 +4,24 @@ const country = 'Brazil'
 
 const pessoa = {
     name,
-    age,
-    country
+    idade: age,
+    pais: country
 }
 
-console.log(pessoa);
+// console.log(pessoa);
+
+// const idade = pessoa.idade;
+// const pais = pessoa.pais;
+
+const { idade, pais, cidade = 'Campo Grande'} = pessoa;
+
+// console.log(idade);
+// console.log(pais);
+// console.log(cidade);
+
+const save = ({name, idade}) => {
+    console.log(name);
+    console.log(idade);
+}
+
+save(pessoa);
