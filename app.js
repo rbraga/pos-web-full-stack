7750c888b83d435230cf7fcdd909aef03ea62500
Rafael <rafabraga85@gmail.com>
2020-03-06 22:38:38 -0400

    desafio yargs

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-const _ = require('lodash');
 const  chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2].toUpperCase();
-const title = process.argv[3];
+// alterando a versão do CLI
+yargs.version('1.0.1');
 
-if (command === 'ADD') {
-    console.log(chalk.green.inverse(`adding new task -> ${title}`));
-} else if (command === 'REMOVE') {
-    console.log(chalk.blue.inverse('removing a task'));
-} else {
-    console.log(chalk.red.inverse('command not found'));
-}
+yargs.command({
+    command: 'add',
+    describe: 'Add a new task into the ToDo list',
+    handler: function() {
+        console.log(chalk.green('Creating a new task'));
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a task from the ToDo list',
+    handler: function() {
+        console.log(chalk.red('Removing a existing task'));
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all tasks',
+    handler: function() {
+        console.log(chalk.blue('Listing out all tasks'));
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a task from the ToDo list',
+    handler: function() {
+        console.log(chalk.yellow('Reading a task'));
+    }
+})
+
+console.log(yargs.argv);
