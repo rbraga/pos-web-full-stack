9ef19c4429310b2ddd8cf7fb08d0c26121c67e5f
Rafael <rafabraga85@gmail.com>
2020-03-07 13:23:47 -0400

    async

---
+++ 02. Programação web com NODE.JS - PWN (2019)/exemplos/async_1.js
@@ -0,0 +1,19 @@
+console.log(' primeiro ');
+
+setTimeout(() => {
+    console.log(' Depois de 2 segundos ')
+
+    setTimeout(() => {
+        console.log(' Depois de 1 segundo ')
+    }, 1000)
+
+    console.log(' outra mensagem depois do tempo ')
+}, 2000);
+
+console.log(' segundo ');
+
+setTimeout(() => {
+    console.log(' 0 segundos ')
+}, 0)
+
+console.log(' terceiro ');
