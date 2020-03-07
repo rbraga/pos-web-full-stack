c1b1116356c21c56e98ed01e5894ff58e00a867c
Rafael <rafabraga85@gmail.com>
2020-03-07 14:05:38 -0400

    desafio callback

--- 02-PWN/exemplos/async/callback.js
+++ 02-PWN/exemplos/async/callback.js
@@ -12,6 +12,17 @@ const getPrice = (symbol, callback) => {
 // const price = getPrice('PETR4.SA');
 // console.log(price);
 
-getPrice('PETR.4', (data) => {
-    console.log(data);
+// getPrice('PETR.4', (data) => {
+//     console.log(data);
+// })
+
+const somaAsync = (valor1, valor2, callback) => {
+    setTimeout(() => {
+        const resultadoSoma = valor1 + valor2;
+        callback(resultadoSoma);
+    }, 3000)
+}
+
+somaAsync(2, 6, (resultadoSoma) => {
+    console.log(resultadoSoma);
 })
