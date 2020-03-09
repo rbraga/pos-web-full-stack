73726387abaa350339a5feffe8e26c7c224fa97d
Rafael <rafabraga85@gmail.com>
2020-03-09 00:30:18 -0400

    express hbs

--- 02-PWN/web-server/src/app.js
+++ 02-PWN/web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Bem vindo ao sistema de cotações',
+        author: 'Rafael Braga'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Sobre',
+        author: 'Rafael Braga'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Ajuda'
+    });
+})
 
 app.get('/cotacoes', (req, res) => {
     const cotacao = { 
