4901123125d64d89188c777be8c07080e56e5703
Rafael <rafabraga85@gmail.com>
2020-03-10 02:57:43 -0400

    todo api 2

--- 02-PWN/todo-api/src/app.js
+++ 02-PWN/todo-api/src/app.js
@@ -1,5 +1,7 @@
 const express  = require('express');
+require('./db/mongoose');
 const userRouter = require('./routes/users');
+const taskRouter = require('./routes/tasks');
 
 const app = express();
 const port = process.env.PORT || 3000;
--- 02-PWN/todo-api/src/routes/users.js
+++ 02-PWN/todo-api/src/routes/users.js
@@ -1,33 +1,79 @@
 const express = require('express');
+const User = require('../models/users');
 
 
 const router = new express.Router();
 
-router.post('/users', (req, res) => {
-    res.status(201).send();
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch(error) {
+        res.status(400).send(error);
+    }
+    res.status(201).send(req.body);
 });
 
-router.get('/users', (req, res) => {
-    const users = [
-        {name:  'user1'},
-        {name:  'user2'}
-    ];
-
-    res.status(200).send(users);
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send(users);
+    } catch(error) {
+        res.status(500).send(error)
+    }
 });
 
-router.get('/users/:id', (req, res) => {
-    console.log(req.params.id);
-    const user = {name: 'user1'};
-    res.status(200).send(user);
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+
+
+    try {
+        const user = await User.findById(_id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user)
+    } catch(error) {
+        res.status(500).send(error)
+    }
 });
 
-router.patch('/users/:id', (req, res) => {
-    res.send();
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdate = ["name", "email", "password", "age"];
+    const isValidOperation = updates.every((update) => allowedUpdate.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Esses campos não podem ser atualizados'});
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error)
+    }
 });
 
-router.delete('users/:id', (req, res) => {
-    res.send();
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch(error) {
+        res.status(500).send();
+    }
 });
 
 module.exports = router;
