c21def2a4525cad9a910d8435eae29ec5c3e6840
Rafael Braga dos Anjos <rafael.anjos@ms.senac.br>
2020-04-12 21:10:14 -0400

    componentDidUpdate

--- 04-react/my-project/src/App.js
+++ 04-react/my-project/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './TodoList';
-import { TodoList } from './TodoList';
+import { Users } from './Users';
 
-function App() {
-  return (
-    <TodoList />
-  );
+class App extends Component {
+  state = {
+    searchForUser: ''
+  } 
+
+  render() {
+    const handleFormSubmit = (event) => {     
+      event.preventDefault()
+      const inputValue = document.getElementById('search-input').value 
+      this.setState({
+        searchForUser: inputValue
+      })
+    }
+
+    return (
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <input id="search-input" placeholder="Search for a user" />
+          <button >Search</button>
+        </form>
+        <Users searchFor={this.state.searchForUser} />
+      </div>
+    );
+  }
 }
 
 export default App;
