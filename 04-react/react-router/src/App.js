38239e8bccae544cd82dccc68b0b8be8880b65f6
Rafael Braga dos Anjos <rafael.anjos@ms.senac.br>
2020-04-13 01:10:33 -0400

    react router

---
+++ 04-react/react-router/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import './App.css';
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function About({ match }) {
+return <h1>About { match.params.name }</h1>
+}
+
+function Contact() {
+  return <h1>Contact</h1>
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about/Rafael">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/about/:name" component={About}></Route>
+      <Route path="/contact" component={Contact}></Route>
+    </BrowserRouter>
+  );
+}
+
+export default App;
