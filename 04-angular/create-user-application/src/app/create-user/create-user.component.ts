e5dfcd774bcf01dff6109c63e16e4d65b6eb61ca
Rafael Braga <rafaelbraga@MacBook-Pro-de-Rafael.local>
2020-04-22 01:49:37 -0400

    forms

---
+++ 04-angular/create-user-application/src/app/create-user/create-user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmitForm(form){
+    console.log(form.value);
+  }
+
+}
