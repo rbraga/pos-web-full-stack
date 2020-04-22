ebefeceee112b3ee3b17adcddcb39fec2a65ac32
Rafael Braga <rafaelbraga@MacBook-Pro-de-Rafael.local>
2020-04-22 00:39:25 -0400

    diretivas

--- 04-angular/angular-app/src/app/my-component/my-component.component.ts
+++ 04-angular/angular-app/src/app/my-component/my-component.component.ts
@@ -5,12 +5,19 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   template: `
     <p>Custom property {{ customProperty }} </p>
     <button (click)="handleCustomEvent()">Send output</button>
+    <ul>
+      <li *ngFor="let item of items">
+        <span *ngIf="item !== 'item 3'">{{item}}</span>
+      </li>
+    </ul>
 `,
   styleUrls: ['./my-component.component.css']
 })
 export class MyComponentComponent {
   @Input() customProperty: string;
   @Output() myCustomOutput = new EventEmitter<string>();
+
+  items = ['item 1', 'item 2', 'item 3', 'item 4']
 
   constructor() { }
 
