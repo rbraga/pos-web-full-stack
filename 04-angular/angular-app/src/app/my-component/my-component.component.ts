b6457a6d4a3cae3a49c35c63e4f876785d41a314
Rafael Braga <rafaelbraga@MacBook-Pro-de-Rafael.local>
2020-04-22 00:58:40 -0400

    two way binding

--- 04-angular/angular-app/src/app/my-component/my-component.component.ts
+++ 04-angular/angular-app/src/app/my-component/my-component.component.ts
@@ -3,7 +3,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-my-component',
   template: `
-    <p>Custom property {{ customProperty }} </p>
+    <p>Custom property: {{ customProperty }} </p>
+    <input type="text" [(ngModel)]="customProperty" />
     <button (click)="handleCustomEvent()">Send output</button>
     <ul>
       <li *ngFor="let item of items">
@@ -22,11 +23,12 @@ export class MyComponentComponent {
   @Output() myCustomOutput = new EventEmitter<string>();
 
   items = ['item 1', 'item 2', 'item 3', 'item 4']
+  inputValue: string;
 
   constructor() { }
 
   handleCustomEvent() {
-    this.myCustomOutput.emit('Sending custom output');
+    this.myCustomOutput.emit(this.inputValue);
   }
 
 }
