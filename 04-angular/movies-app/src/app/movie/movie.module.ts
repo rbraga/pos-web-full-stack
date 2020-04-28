9d9b23bacff6cd3f04070097eb1a1de5f7aae3e1
Rafael Braga <rafaelbraga@MacBook-Pro-de-Rafael.local>
2020-04-28 00:22:52 -0400

    modules

--- 04-angular/movies-app/src/app/movie/movie.module.ts
+++ 04-angular/movies-app/src/app/movie/movie.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieItemComponent } from 'movie-item/movie-item.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
 
 
 
