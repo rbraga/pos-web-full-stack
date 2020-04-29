2868497171f0c3a28c5f5559190112f773a3f248
Rafael Braga <rafabraga85@gmail.com>
2020-04-29 01:10:40 -0400

    gerenciando subscriptions

--- 04-angular/movies-app/src/app/movie/movies-list/movies-list.component.ts
+++ 04-angular/movies-app/src/app/movie/movies-list/movies-list.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MoviesService } from '../movies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css']
 })
-export class MoviesListComponent implements OnInit {
+export class MoviesListComponent implements OnInit, OnDestroy {
   movies: [];
+  subscription: Subscription;
 
   constructor(
     private moviesService: MoviesService
   ) { }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.moviesService.getPopularMovies()
+    this.subscription = this.moviesService.getPopularMovies()
       .subscribe((response: any) => {
         this.movies = response;
       })
   }
-
 }
