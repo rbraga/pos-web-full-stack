ec3a518798efc15c338bf7e9b195d04e73091550
Rafael Braga <rafabraga85@gmail.com>
2020-05-10 17:08:41 -0400

    criando banco de dados

---
+++ 05-php/php_laravel/database/migrations/2020_05_10_210016_create_despesas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despesas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descricao');
+            $table->datetime('data');
+            $table->double('valor');
+
+            $table->integer('categoria_id')->unsigned();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despesas');
+    }
+}
